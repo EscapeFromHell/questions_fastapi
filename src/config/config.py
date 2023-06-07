f05d4d96dff56ac7586cfddfa73446fcc14abb3e
EscapeFromHell <kirill199752@yandex.ru>
2023-06-07 13:41:55 +0400

    init_structure

---
+++ src/config/config.py
@@ -0,0 +1,30 @@
+from pydantic import AnyHttpUrl, BaseSettings, validator
+
+
+class Settings(BaseSettings):
+    API_V1_STR: str = "/api_v1"
+    BACKEND_CORS_ORIGINS: list[AnyHttpUrl] = [
+        "http://localhost",
+        "http://127.0.0.1",
+    ]
+    BACKEND_HOST_ORIGINS: list[AnyHttpUrl] = [
+        "http://localhost",
+        "http://127.0.0.1",
+    ]
+
+    @validator("BACKEND_CORS_ORIGINS", pre=True)
+    def assemble_cors_origins(cls, v: str | list[str]) -> list[str] | str:
+        if isinstance(v, str) and not v.startswith("["):
+            return [i.strip() for i in v.split(",")]
+        elif isinstance(v, (list, str)):
+            return v
+        raise ValueError(v)
+
+    DATABASE_DSN: str = "postgresql+psycopg2://postgres:password@questions-fastapi-db:5432/questions-fastapi"
+    QUESTION_URL: str = "https://jservice.io/api/random"
+
+    class Config:
+        case_sensitive = True
+
+
+settings = Settings()
